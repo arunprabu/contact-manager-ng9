df5560a680fe0ad6e9485f119016c735c359a890
Arun Vijayarengan <arunprabu.v@hotmail.com>
2020-06-22 18:24:06 +0530

    day 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', // exposed in a selector -- an element selector -- mandatory
+  templateUrl: './app.component.html',  // html -- mandatory -- should be only one template
+  styleUrls: ['./app.component.css']  // css -- optional -- can be multiple as well
 })
 export class AppComponent {
+  // ts
   title = 'contact-manager-ng9';
 }
