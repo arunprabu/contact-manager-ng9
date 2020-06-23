1a04b67c62fbb55098bae3d699e8261d6d87f3fb
Arun Vijayarengan <arunprabu.v@hotmail.com>
2020-06-23 16:08:39 +0530

    day 2

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -2,7 +2,17 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  templateUrl: './footer.component.html',
+  template: `
+    <div class="text-center">
+      <hr>
+      <app-menu>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Back to Top</a>
+        </li>
+      </app-menu>
+      <p>Copyright - Arun | 2020</p>
+    </div>
+  `,
   styles: [
   ]
 })
